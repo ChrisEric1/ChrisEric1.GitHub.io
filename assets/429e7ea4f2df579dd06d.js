3ee49426759d998f5066a5d85f5ea754604a912a
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2023-12-02 14:57:48 -0500

    Move assets to own repo to prevent legal issues later

--- assets/429e7ea4f2df579dd06d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{10028:function(n,o){},10030:function(n,o){},10060:function(n,o){},10061:function(n,o){}}]);
-//# sourceMappingURL=429e7ea4f2df579dd06d.js.map
